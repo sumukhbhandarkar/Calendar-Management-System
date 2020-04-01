d5da8fb5e69db740cdfe02d3a4490ca8518f8a2b
Sumukh Bhandarkar <sumukhbhandarkar@gmail.com>
2020-04-01 13:22:55 +0530

    First Commit, Spring Startr

---
+++ src/main/java/com/postman/assignment/cms/CalendarMgmtSystem/CalendarMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.postman.assignment.cms.CalendarMgmtSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarMgmtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postman/assignment/cms/CalendarMgmtSystem/CalendarMgmtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postman.assignment.cms.CalendarMgmtSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarMgmtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
